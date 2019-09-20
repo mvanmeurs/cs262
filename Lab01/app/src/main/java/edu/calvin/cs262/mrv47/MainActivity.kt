20fdd87c2fab8dd009d69cc6f107b13a7ab3a2e7
mrv47 <mrv47@gold31.cs.calvin.edu>
2019-09-20 14:49:36 -0400

    Lab01 and 02 initial commit

---
+++ Lab01/app/src/main/java/edu/calvin/cs262/mrv47/MainActivity.kt
@@ -0,0 +1,14 @@
+package edu.calvin.cs262.mrv47
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Hello, Mrv47!")
+    }
+}
