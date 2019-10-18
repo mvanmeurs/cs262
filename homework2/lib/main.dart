a61a95fd8de8a1f54598277bddd4f7bba3e46e87
mvanmeurs <masonvanmeurs7@gmail.com>
2019-10-18 12:05:35 -0400

    Initial commit homework2

---
+++ homework2/lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(SharedPreferenceDemo());
+
+class SharedPreferenceDemo extends StatefulWidget {
+  SharedPreferenceDemo() : super();
+
+  final String title = "Homework 2";
+
+  @override
+  SharedPreferenceDemoState createState() => SharedPreferenceDemoState();
+}
+
+class SharedPreferenceDemoState extends State<SharedPreferenceDemo> {
+  //
+  String data = "";
+  String nameKey = "_key_name";
+  TextEditingController controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    const MethodChannel('plugins.flutter.io/shared_preferences')
+        .setMockMethodCallHandler(
+          (MethodCall methodcall) async {
+        if (methodcall.method == 'getAll') {
+          return {"flutter." + nameKey: "[ No Name Saved ]"};
+        }
+        return null;
+      },
+    );
+    setData();
+  }
+
+  Future<bool> saveData() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return await preferences.setString(nameKey, controller.text);
+  }
+
+  Future<String> loadData() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString(nameKey);
+  }
+
+  setData() {
+    loadData().then((value) {
+      setState(() {
+        data = value;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Homework2',
+      home: Scaffold (
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              TextField(
+                controller: controller,
+                decoration: InputDecoration.collapsed(hintText: "Enter Name"),
+              ),
+              OutlineButton(
+                child: Text("SAVE NAME"),
+                onPressed: () {
+                  saveData();
+                },
+              ),
+              Text(
+                data,
+                style: TextStyle(fontSize: 20),
+              ),
+              OutlineButton(
+                child: Text("LOAD NAME"),
+                onPressed: () {
+                  setData();
+                },
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
